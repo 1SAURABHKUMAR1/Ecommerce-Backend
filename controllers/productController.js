99a5025454a15fe706620b1b0bf8b12e51df22e6
saurabhku463 <saurabhku463@gmail.com>
2022-04-18 13:16:52 +0530

    get all products user

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,50 +1,46 @@
-const Product = require("../models/product");
-const BigPromise = require("../middleware/bigPromise");
-const CustomError = require("../utils/customError");
-const cloudinary = require("cloudinary").v2;
-const WhereClause = require("../utils/whereClause");
-const mongoose = require("mongoose");
-
+const Product = require('../models/product');
+const BigPromise = require('../middleware/bigPromise');
+const CustomError = require('../utils/customError');
+const cloudinary = require('cloudinary').v2;
+const WhereClause = require('../utils/whereClause');
+const mongoose = require('mongoose');
 
 // add product
 exports.addProduct = BigPromise(async (req, res, next) => {
-
     const { name, price, description, category, stock, brand } = req.body;
 
     // any field is missing
     if (!(name && price && description && category && stock && brand)) {
-        return next(CustomError(res, "All Fields Are Mandatory!", 400));
+        return next(CustomError(res, 'All Fields Are Mandatory!', 400));
     }
 
     // product is already in db
     const productDB = await Product.findOne({ name });
 
     if (productDB) {
-        return next(CustomError(res, "Product Name Already Exists", 400));
+        return next(CustomError(res, 'Product Name Already Exists', 400));
     }
 
     // no photo is passed
     if (!req.files) {
-        return next(CustomError(res, "Photo is required", 401));
+        return next(CustomError(res, 'Photo is required', 401));
     }
 
     // upload photo
     let imagesArray = [];
 
     for (let index = 0; index < req.files.photos.length; index++) {
-
         let result = await cloudinary.uploader.upload(
             req.files.photos[index].tempFilePath,
             {
-                folder: "ecommerce",
-            }
+                folder: 'ecommerce',
+            },
         );
 
         imagesArray.push({
             id: result.public_id,
             secure_url: result.secure_url,
         });
-
     }
 
     // create product model
@@ -64,13 +60,10 @@ exports.addProduct = BigPromise(async (req, res, next) => {
         success: true,
         product,
     });
-
-})
-
+});
 
 // filter product -- if no query return all product else filter via query
 exports.filterProduct = BigPromise(async (req, res, next) => {
-
     // result in one page
     let resultPerPage = 3;
 
@@ -83,7 +76,9 @@ exports.filterProduct = BigPromise(async (req, res, next) => {
     const totalProducts = await Product.countDocuments();
 
     // filter via query
-    const productObj = new WhereClause(Product.find(), req.query).search().filter();
+    const productObj = new WhereClause(Product.find(), req.query)
+        .search()
+        .filter();
 
     // total filtered products
     let product = await productObj.baseURL;
@@ -100,37 +95,31 @@ exports.filterProduct = BigPromise(async (req, res, next) => {
         resultPerPage,
         totalFilteredResults,
         totalProducts,
-    })
-
-})
-
+    });
+});
 
 // get all products admin
-exports.adminGetAllProducts = BigPromise(async (req, res, next) => {
-
+exports.getAllProducts = BigPromise(async (req, res, next) => {
     const products = await Product.find();
 
     res.status(200).json({
         success: true,
         products,
-    })
-
+    });
 });
-
 
 // get single product
 exports.getSingleProduct = BigPromise(async (req, res, next) => {
-
     const productId = req.params.id;
 
     // product is is not passed
     if (!productId) {
-        return next(CustomError(res, "Product Id Is Required!", 401));
+        return next(CustomError(res, 'Product Id Is Required!', 401));
     }
 
-    // if not bson 
+    // if not bson
     if (!mongoose.Types.ObjectId.isValid(productId)) {
-        return next(CustomError(res, "Product Id not valid!", 400));
+        return next(CustomError(res, 'Product Id not valid!', 400));
     }
 
     // find all data
@@ -138,44 +127,40 @@ exports.getSingleProduct = BigPromise(async (req, res, next) => {
 
     // no product found
     if (!product) {
-        return next(CustomError(res, "No Product Found!", 401));
+        return next(CustomError(res, 'No Product Found!', 401));
     }
 
     res.status(200).json({
         success: true,
-        product
+        product,
     });
-
 });
-
 
 // update information of product
 exports.adminUpdateProductInfo = BigPromise(async (req, res, next) => {
-
     const productId = req.params.id;
 
     // id not passed
     if (!productId) {
-        return next(CustomError(res, "Product ID Is Required!", 401));
+        return next(CustomError(res, 'Product ID Is Required!', 401));
     }
 
-    // if not bson 
+    // if not bson
     if (!mongoose.Types.ObjectId.isValid(productId)) {
-        return next(CustomError(res, "Product Id not valid!", 400));
+        return next(CustomError(res, 'Product Id not valid!', 400));
     }
 
     let product = await Product.findById(productId);
 
     // product not found in db
     if (!product) {
-        return next(CustomError(res, "Product Not Found", 400));
+        return next(CustomError(res, 'Product Not Found', 400));
     }
 
     // if photo is present
     const imagesArray = [];
 
     if (req.files) {
-
         // delete existing photos
         const existingPhotos = product.photos;
         for (let index = 0; index < existingPhotos.length; index++) {
@@ -187,24 +172,22 @@ exports.adminUpdateProductInfo = BigPromise(async (req, res, next) => {
             const imageUpload = await cloudinary.uploader.upload(
                 req.files.photos[index].tempFilePath,
                 {
-                    folder: "ecommerce",
-                }
-            )
+                    folder: 'ecommerce',
+                },
+            );
 
             imagesArray.push({
                 id: imageUpload.public_id,
                 secure_url: imageUpload.secure_url,
-            })
+            });
         }
-
     }
 
     // update user in body
     if (imagesArray.length > 0) {
-
         // image array is not empty
         req.body.photos = imagesArray;
-        console.log("image Array");
+        console.log('image Array');
     }
 
     req.body.user = req.user.id;
@@ -218,30 +201,27 @@ exports.adminUpdateProductInfo = BigPromise(async (req, res, next) => {
     res.status(200).json({
         success: true,
         product,
-    })
-
+    });
 });
-
 
 // admin delete product
 exports.adminDeleteProduct = BigPromise(async (req, res, next) => {
-
     const productId = req.params.id;
 
     if (!productId) {
-        return next(CustomError(res, "Product ID Is Required!", 401));
+        return next(CustomError(res, 'Product ID Is Required!', 401));
     }
 
-    // if not bson 
+    // if not bson
     if (!mongoose.Types.ObjectId.isValid(productId)) {
-        return next(CustomError(res, "Product Id not valid!", 400));
+        return next(CustomError(res, 'Product Id not valid!', 400));
     }
 
     const product = await Product.findById(productId);
 
     // product id is invalid
     if (!product) {
-        return next(CustomError(res, "Product Not Found!", 400));
+        return next(CustomError(res, 'Product Not Found!', 400));
     }
 
     // delete all photos of product
@@ -254,26 +234,24 @@ exports.adminDeleteProduct = BigPromise(async (req, res, next) => {
 
     res.status(200).json({
         success: true,
-        message: "Product Deleted Successfully!",
-    })
+        message: 'Product Deleted Successfully!',
+    });
 });
-
 
 // add review on product - if review change the review
 exports.addReview = BigPromise(async (req, res, next) => {
-
     const { rating, comment, productId } = req.body;
 
     // all fields required
     if (!(rating && comment && productId)) {
-        return next(CustomError(res, "All Fields Are Required!", 400));
+        return next(CustomError(res, 'All Fields Are Required!', 400));
     }
 
     const product = await Product.findById(productId);
 
     // productID is invalid
     if (!product) {
-        return next(CustomError(res, "Product Not Found!", 400));
+        return next(CustomError(res, 'Product Not Found!', 400));
     }
 
     //  reiew of user
@@ -282,36 +260,36 @@ exports.addReview = BigPromise(async (req, res, next) => {
         name: req.user.name,
         rating: Number(rating),
         comment,
-    }
+    };
 
     // check if user already added
     const AlreadyCommented = product.reviews.find(
-        (item) => item.user.toString() === req.user._id.toString()
+        (item) => item.user.toString() === req.user._id.toString(),
     );
 
     //     // if review already exits by user
     if (AlreadyCommented) {
-
         product.reviews.forEach((item) => {
             if (item.user.toString() === req.user._id.toString()) {
                 item.rating = rating;
                 item.comment = comment;
             }
-        })
-    }
-    else {
-
+        });
+    } else {
         // if user havent given any review
         product.reviews.push(review);
         product.numberOfReviews = product.reviews.length;
     }
 
     // change average rating
-    product.rating = product.reviews.reduce((previousItem, currentItem) => currentItem.rating + previousItem, 0) / product.reviews.length;
+    product.rating =
+        product.reviews.reduce(
+            (previousItem, currentItem) => currentItem.rating + previousItem,
+            0,
+        ) / product.reviews.length;
 
     // save user model
     await product.save({ validateBeforeSave: false });
-
 
     res.status(200).json({
         success: true,
@@ -319,71 +297,75 @@ exports.addReview = BigPromise(async (req, res, next) => {
     });
 });
 
-
 // delete a review
 exports.deleteReview = BigPromise(async (req, res, next) => {
-
     const productId = req.query.id;
 
     if (!productId) {
-        return next(CustomError(res, "All Fields Are Mandatory!", 400));
+        return next(CustomError(res, 'All Fields Are Mandatory!', 400));
     }
 
     let product = await Product.findById(productId);
 
     if (!product) {
-        return next(CustomError(res, "Product Not Found!", 400));
+        return next(CustomError(res, 'Product Not Found!', 400));
     }
 
     // new review array with deleted user review
     const reviews = product.reviews.filter(
-        (item) => item.user.toString() !== req.user._id.toString()
+        (item) => item.user.toString() !== req.user._id.toString(),
     );
 
     const numberOfReviews = reviews.length;
 
     // average rating
-    const rating = numberOfReviews > 0 ? reviews.reduce((previousItem, currentItem) => previousItem + currentItem.rating, 0) / numberOfReviews : 0;
+    const rating =
+        numberOfReviews > 0
+            ? reviews.reduce(
+                  (previousItem, currentItem) =>
+                      previousItem + currentItem.rating,
+                  0,
+              ) / numberOfReviews
+            : 0;
 
     // update the product
-    product = await Product.findByIdAndUpdate(productId, {
-        rating,
-        numberOfReviews,
-        reviews,
-    }, {
-        new: true,
-        runValidators: true,
-    })
+    product = await Product.findByIdAndUpdate(
+        productId,
+        {
+            rating,
+            numberOfReviews,
+            reviews,
+        },
+        {
+            new: true,
+            runValidators: true,
+        },
+    );
 
     res.status(200).json({
         success: true,
         reviews: product.reviews,
-    })
-
+    });
 });
-
 
 // send all the review of product
 exports.getSingleProductReviews = BigPromise(async (req, res, next) => {
-
     const productId = req.query.id;
 
     // product id not passed
     if (!productId) {
-        return next(CustomError(res, "All Fields Are Mandatory!", 400));
+        return next(CustomError(res, 'All Fields Are Mandatory!', 400));
     }
 
     const product = await Product.findById(productId);
 
     // product not found
     if (!product) {
-        return next(CustomError(res, "Product Not Found", 400));
+        return next(CustomError(res, 'Product Not Found', 400));
     }
 
     res.status(200).json({
         success: true,
         reviews: product.reviews,
-    })
-
-})
-
+    });
+});
--- routes/product.js
+++ routes/product.js
@@ -1,42 +1,44 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
 // middleware
-const { isLoggedIn, customRole } = require("../middleware/user")
-
+const { isLoggedIn, customRole } = require('../middleware/user');
 
 // controller of product
 const {
     addProduct,
     filterProduct,
-    adminGetAllProducts,
+    getAllProducts,
     getSingleProduct,
     adminUpdateProductInfo,
     adminDeleteProduct,
     addReview,
     deleteReview,
     getSingleProductReviews,
-} = require("../controllers/productController");
-
+} = require('../controllers/productController');
 
 // product route
-router.route("/product").get(filterProduct);
-router.route("/product/:id").get(getSingleProduct);
-
+router.route('/product').get(filterProduct);
+router.route('/product/:id').get(getSingleProduct);
+router.route('/admin/products').get(isLoggedIn, getAllProducts);
 
 // product route for admin
-router.route("/admin/product/add").post(isLoggedIn, customRole('admin'), addProduct);
-router.route("/admin/products").get(isLoggedIn, customRole('admin'), adminGetAllProducts);
-router.route("/admin/product/:id")
+router
+    .route('/admin/product/add')
+    .post(isLoggedIn, customRole('admin'), addProduct);
+router
+    .route('/admin/products')
+    .get(isLoggedIn, customRole('admin'), getAllProducts);
+router
+    .route('/admin/product/:id')
     .put(isLoggedIn, customRole('admin'), adminUpdateProductInfo)
-    .delete(isLoggedIn, customRole("admin"), adminDeleteProduct);
-
+    .delete(isLoggedIn, customRole('admin'), adminDeleteProduct);
 
 // review routes
-router.route("/review")
+router
+    .route('/review')
     .put(isLoggedIn, addReview)
     .delete(isLoggedIn, deleteReview);
-router.route("/reviews").get(getSingleProductReviews);
-
+router.route('/reviews').get(getSingleProductReviews);
 
 module.exports = router;
