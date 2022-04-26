a7749ea198040e9b6b2a5c479635d6dbb93c92b6
saurabhku463 <saurabhku463@gmail.com>
2022-04-26 23:35:26 +0530

    small changes

--- middleware/user.js
+++ middleware/user.js
@@ -1,22 +1,21 @@
-const BigPromise = require("./bigPromise");
-const User = require("../models/user");
-const CustomError = require("../utils/customError");
-const jwt = require("jsonwebtoken");
+const BigPromise = require('./bigPromise');
+const User = require('../models/user');
+const CustomError = require('../utils/customError');
+const jwt = require('jsonwebtoken');
 
 // middleware for checking if user has token and is logged in or not
 exports.isLoggedIn = BigPromise(async (req, res, next) => {
-
     // getting token from header
     let token = req.cookies.token;
 
     // if token is not present in cookie but in header
-    if (!token && req.header("Authorization")) {
-        token = req.header("Authorization").replace("Bearer", "");
+    if (!token && req.header('Authorization')) {
+        token = req.header('Authorization').replace('Bearer', '');
     }
 
     // token not present in both cookie and header
     if (!token) {
-        return next(CustomError(res, "Login To Proceded", 400));
+        return next(CustomError(res, 'Login To Proceded', 400));
     }
 
     // decode the payload from jwt
@@ -28,17 +27,20 @@ exports.isLoggedIn = BigPromise(async (req, res, next) => {
     next();
 });
 
-
 // middleware for checking specific role eg- admin , manager
 exports.customRole = (...role) => {
-
-    // ...role converts it into a array 
-    // eg - we send admin --> [admin] and we can check if req.user.role --> admin is in [admin] 
+    // ...role converts it into a array
+    // eg - we send admin --> [admin] and we can check if req.user.role --> admin is in [admin]
     return (req, res, next) => {
         if (!role.includes(req.user.role)) {
-            return next(CustomError(res, "You are not authorized to visit this page!", 403));
+            return next(
+                CustomError(
+                    res,
+                    'You are not authorized to visit this page!',
+                    403,
+                ),
+            );
         }
         next();
-    }
-
-}
+    };
+};
